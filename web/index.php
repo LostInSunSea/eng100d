71b2a7e6dfd425ab8ef0de8a355c15e0abcbc35e
Austin Sun <austinsunc@yahoo.com>
2016-06-01 12:33:25 -0700

    fixed index page

--- web/index.php
+++ web/index.php
@@ -15,7 +15,8 @@
         <link rel="stylesheet" href="css/materialize.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
-    <body class="homescreen-body">
+    <body class="homescreen-body" style="margin-top: -30px">
+      <div class="container">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -27,7 +28,7 @@
 
         <!-- logo -->
         <div class="logo">
-            <center><img class="homescreen-logo" src="img/WWF-Logo.png"></center>
+            <center><img class="homescreen-logo responsive-img" src="img/WWF-Logo.png"></center>
         </div>
 
         <!-- spacing -->
@@ -36,11 +37,17 @@
 
         <!-- signin button -->
         <div class="button">
-            <center><button homescreen onclick="window.location.href='signinform.html'" class="homescreen-btn homescreen-signin waves-effect waves-teal">SIGN IN</button></td></center>
+            <center><button homescreen onclick="window.location.href='signinform.html'" class="homescreen-btn homescreen-signin waves-effect waves-teal">Returning Volunteer</button></td></center>
         </div>
 
         <div class="homescreen-bot-divider"></div>
 
+        <!-- signin button -->
+        <div class="button">
+           <center><button homescreen onclick="window.location.href='newvolunteer.html'" class="homescreen-btn homescreen-signin waves-effect waves-teal">New Volunteer</button></td></center>
+        </div>
+
+                <div class="homescreen-bot-divider"></div>
         <!-- signout button -->
         <div class="button">
             <center>
@@ -50,7 +57,7 @@
               </td>
             </center>
         </div>
-
+    </div>
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>

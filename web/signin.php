36e47543de96f4c2dcbb722fc4e213a3b5890fb5
Austin Sun <austinsunc@yahoo.com>
2016-07-20 15:44:07 -0700

    more signin

---
+++ web/signin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: austinsun
+ * Date: 7/20/16
+ * Time: 3:17 PM
+ */
+$csv=$_POST['line'];
+$index=$_POST['index'];
+$file = fopen("Volunteers.csv","r");
+

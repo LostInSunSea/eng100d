3db67531bfb8c9c54ba4cf9af7c8da7cf75d9195
Alex Tang <atang1994@gmail.com>
2016-07-26 23:03:29 -0700

    some mysql query to update start time

--- web/signin.php
+++ web/signin.php
@@ -8,4 +8,17 @@
 $csv=$_POST['line'];
 $index=$_POST['index'];
 $file = fopen("Volunteers.csv","r");
+fclose($file);
 
+/**
+ * writes time to output.csv file
+ * does not work
+ */
+$f2 = fopen("output.csv", "w");
+$time = strftime("%Y. %B %d. %A. %X %Z");
+
+fputcsv($f2, explode(',', $time));
+fclose($f2);
+
+return $time;
+?>
